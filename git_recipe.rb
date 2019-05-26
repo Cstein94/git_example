147bdda71f4c72320e9ca57e4075c92f9c7bd6ae
Cstein94 <chymowitz94@gmail.com>
2019-05-26 09:31:14 -0500

    finish recipe

--- git_recipe.rb
+++ git_recipe.rb
@@ -1,3 +1,5 @@
-Make a PBJ
-Toast two peices of bread
-On one peice spread peanut butter
+Make a PBJ :
+1) Toast two peices of bread
+2) On one peice spread peanut butter
+3) On the other spead jelly
+4) Put the two peices together.
