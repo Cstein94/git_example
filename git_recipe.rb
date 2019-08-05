2edda83dd6a6aac84336b17dd55baf85fa1c52b4
Cstein94 <chymowitz94@gmail.com>
2019-08-04 20:04:38 -0500

    Commiting can be fun

--- git_recipe.rb
+++ git_recipe.rb
@@ -1,10 +1,9 @@
-Make a PBJ :
-1) Toast two peices of bread
-2) On one peice spread peanut butter
-3) On the other spead jelly
-4) Put the two peices together.
+def cookbook
+	recipe = "apple pie"
+	ingredients = ['Apples', 'Sugar', 'Deliciousness']
+	final_product = "We are going to make #{recipe} and we will need #{ingredients[0]}, #{ingredients[1]}, and #{ingredients[2]}."
+	puts final_product
+end
 
+cookbook
 
-
-
-EVERTHING IS AWESOME IN GITHUB
